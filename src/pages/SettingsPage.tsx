2fd301caef1be6f0cd42250813b709ad42b405ad
Riaan Grobler <riaan@groblers.co.uk>
2026-04-08 13:23:29 +0000

    chore: Bump version to v0.3.0

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import '../styles/Pages.css';
 
-const APP_VERSION = '0.2.0';
+const APP_VERSION = '0.3.0';
 
 export const SettingsPage: React.FC = () => {
   const [settings, setSettings] = useState({
