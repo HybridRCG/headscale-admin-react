26e133093f99eed1661803fcec1269eff233b042
Riaan Grobler <riaan@groblers.co.uk>
2026-04-14 09:19:22 +0200

    fix: add missing useAuthStore import to UsersPage

--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { useAuthStore } from '../store/authStore';
 import '../styles/Pages.css';
 
 interface User {
